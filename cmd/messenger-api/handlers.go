91f8b59b1415f9ad86fd5b77304292bef3227b4c
asciishell <asciishell@yandex.ru>
2019-09-01 21:33:21 +0300

    Implement handlers

--- cmd/messenger-api/handlers.go
+++ cmd/messenger-api/handlers.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/asciishell/avito-backend/internal/chat"
+	"github.com/asciishell/avito-backend/internal/postgresqldb"
 	"github.com/asciishell/avito-backend/internal/storage"
+	"github.com/asciishell/avito-backend/internal/user"
 	"github.com/asciishell/avito-backend/pkg/log"
 )
 
@@ -18,17 +22,98 @@ func NewHandler(l log.Logger, s storage.Storage) *Handler {
 }
 
 func (h Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "not implemented", http.StatusNotFound)
+	var userData user.User
+	if err := json.NewDecoder(r.Body).Decode(&userData); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if err := h.storage.CreateUser(&userData); err != nil {
+		h.logger.Errorf("can't create user: %+v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := w.Write(userData.ShortJSON()); err != nil {
+		h.logger.Errorf("can't write user info: %+v", err)
+		return
+	}
 }
 func (h Handler) CreateChat(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "not implemented", http.StatusNotFound)
+	var chatData chat.Chat
+	if err := json.NewDecoder(r.Body).Decode(&chatData); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if err := h.storage.CreateChat(&chatData); err != nil {
+		h.logger.Errorf("can't create chat: %+v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := w.Write(chatData.ShortJSON()); err != nil {
+		h.logger.Errorf("can't write chat info: %+v", err)
+		return
+	}
 }
 func (h Handler) CreateMessage(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "not implemented", http.StatusNotFound)
+	var chatData chat.Chat
+	if err := json.NewDecoder(r.Body).Decode(&chatData); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if err := h.storage.CreateChat(&chatData); err != nil {
+		h.logger.Errorf("can't create chat: %+v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if _, err := w.Write(chatData.ShortJSON()); err != nil {
+		h.logger.Errorf("can't write message info: %+v", err)
+		return
+	}
 }
 func (h Handler) GetChats(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "not implemented", http.StatusNotFound)
+	var userData user.User
+	if err := json.NewDecoder(r.Body).Decode(&userData); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	chats, err := h.storage.GetChatsFor(userData)
+	if err != nil {
+		switch err {
+		case postgresqldb.ErrNotFound:
+			w.WriteHeader(http.StatusNotFound)
+		default:
+			h.logger.Errorf("can't get chats for %v: %+v", userData, err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+	err = json.NewEncoder(w).Encode(chats)
+	if err != nil {
+		h.logger.Errorf("can't write message info: %+v", err)
+		return
+	}
 }
 func (h Handler) GetMessages(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "not implemented", http.StatusNotFound)
+	var chatData chat.Chat
+	if err := json.NewDecoder(r.Body).Decode(&chatData); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	messages, err := h.storage.GetMessages(chatData)
+	if err != nil {
+		switch err {
+		case postgresqldb.ErrNotFound:
+			w.WriteHeader(http.StatusNotFound)
+		default:
+			h.logger.Errorf("can't get messages for %v: %+v", chatData, err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+	err = json.NewEncoder(w).Encode(messages)
+	if err != nil {
+		h.logger.Errorf("can't write message info: %+v", err)
+		return
+	}
 }
