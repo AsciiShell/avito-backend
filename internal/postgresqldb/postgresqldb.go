bf5ea3729a2d8cc77456ee66ff1752376bed499c
asciishell <asciishell@yandex.ru>
2019-09-01 17:11:31 +0300

    Optimize query

--- internal/postgresqldb/postgresqldb.go
+++ internal/postgresqldb/postgresqldb.go
@@ -81,44 +81,27 @@ func (p *PostgresStorage) CreateMessage(m *message.Message) error {
 }
 
 func (p *PostgresStorage) GetChatsFor(u user.User) ([]chat.Chat, error) {
-	rows, err := p.DB.Raw(`SELECT chats.id, chats.name, chats.created_at, (SELECT MAX(created_at) FROM messages WHERE messages.chat_id = chats.id) as last_message
+	var result []chat.Chat
+	if err := p.DB.Raw(`SELECT chats.id, chats.name, chats.created_at, (SELECT MAX(created_at) FROM messages WHERE messages.chat_id = chats.id) as last_message
 FROM chats
          JOIN user_chats uc on chats.id = uc.chat_id
 WHERE uc.user_id = ?
 ORDER BY last_message DESC 
-`, u.ID).Rows()
-	if err != nil {
+`, u.ID).Scan(&result).Error; err != nil {
 		return nil, errors.Wrapf(err, "can't get chats for user id %v", u.ID)
-	}
-	defer rows.Close()
-	var result []chat.Chat
-	for rows.Next() {
-		var ch chat.Chat
-		if err := p.DB.ScanRows(rows, &ch); err != nil {
-			return nil, errors.Wrapf(err, "can't read from row, client id %v", u.ID)
-		}
-		result = append(result, ch)
 	}
 	return result, nil
 }
 
 func (p *PostgresStorage) GetMessages(c chat.Chat) ([]message.Message, error) {
-	rows, err := p.DB.Raw(`SELECT *
+	var result []message.Message
+
+	if err := p.DB.Raw(`SELECT *
 FROM messages
 WHERE chat_id = ?
 ORDER BY created_at
-`, c.ID).Rows()
-	if err != nil {
-		return nil, errors.Wrapf(err, "can't get messages for chat id %v", c.ID)
-	}
-	defer rows.Close()
-	var result []message.Message
-	for rows.Next() {
-		var m message.Message
-		if err := p.DB.ScanRows(rows, &m); err != nil {
-			return nil, errors.Wrapf(err, "can't read from row, chat id %v", c.ID)
-		}
-		result = append(result, m)
+`, c.ID).Scan(&result).Error; err != nil {
+		return nil, errors.Wrapf(err, "can't read from row, chat id %v", c.ID)
 	}
 	return result, nil
 }
