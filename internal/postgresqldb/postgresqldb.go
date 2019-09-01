ab60712a8f5cf2c2496a24b5978fb2e8b46c9c34
asciishell <asciishell@yandex.ru>
2019-09-01 17:01:35 +0300

    Create handlers

--- internal/postgresqldb/postgresqldb.go
+++ internal/postgresqldb/postgresqldb.go
@@ -53,25 +53,72 @@ func (p *PostgresStorage) Migrate() error {
 }
 
 func (p *PostgresStorage) CreateUser(u *user.User) error {
-	panic("implement me")
+	if err := p.DB.Create(u).Error; err != nil {
+		return errors.Wrapf(err, "can't create user %+v", u)
+	}
+	return nil
 }
 
 func (p *PostgresStorage) GetUser(u *user.User) error {
-	panic("implement me")
+	if err := p.DB.Where(u).First(u).Error; err != nil {
+		return errors.Wrapf(err, "can't get user %+v", u)
+	}
+	return nil
 }
 
 func (p *PostgresStorage) CreateChat(c *chat.Chat) error {
-	panic("implement me")
+	if err := p.DB.Create(c).Error; err != nil {
+		return errors.Wrapf(err, "can't create chat %+v", c)
+	}
+	return nil
 }
 
 func (p *PostgresStorage) CreateMessage(m *message.Message) error {
-	panic("implement me")
+	if err := p.DB.Create(m).Error; err != nil {
+		return errors.Wrapf(err, "can't create message %+v", m)
+	}
+	return nil
 }
 
 func (p *PostgresStorage) GetChatsFor(u user.User) ([]chat.Chat, error) {
-	panic("implement me")
+	rows, err := p.DB.Raw(`SELECT chats.id, chats.name, chats.created_at, (SELECT MAX(created_at) FROM messages WHERE messages.chat_id = chats.id) as last_message
+FROM chats
+         JOIN user_chats uc on chats.id = uc.chat_id
+WHERE uc.user_id = ?
+ORDER BY last_message DESC 
+`, u.ID).Rows()
+	if err != nil {
+		return nil, errors.Wrapf(err, "can't get chats for user id %v", u.ID)
+	}
+	defer rows.Close()
+	var result []chat.Chat
+	for rows.Next() {
+		var ch chat.Chat
+		if err := p.DB.ScanRows(rows, &ch); err != nil {
+			return nil, errors.Wrapf(err, "can't read from row, client id %v", u.ID)
+		}
+		result = append(result, ch)
+	}
+	return result, nil
 }
 
 func (p *PostgresStorage) GetMessages(c chat.Chat) ([]message.Message, error) {
-	panic("implement me")
+	rows, err := p.DB.Raw(`SELECT *
+FROM messages
+WHERE chat_id = ?
+ORDER BY created_at
+`, c.ID).Rows()
+	if err != nil {
+		return nil, errors.Wrapf(err, "can't get messages for chat id %v", c.ID)
+	}
+	defer rows.Close()
+	var result []message.Message
+	for rows.Next() {
+		var m message.Message
+		if err := p.DB.ScanRows(rows, &m); err != nil {
+			return nil, errors.Wrapf(err, "can't read from row, chat id %v", c.ID)
+		}
+		result = append(result, m)
+	}
+	return result, nil
 }
